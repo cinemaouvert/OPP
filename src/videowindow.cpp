cc62c12ce065a3165c6a9991aae49c40c839bafa
ThibaudL <lamarchethibaud@hotmail.fr>
2014-06-06 17:28:48 +0200

    improvement of the close event from videoWindow

--- src/videowindow.cpp
+++ src/videowindow.cpp
@@ -76,6 +76,5 @@ void VideoWindow::moveToDisplay(const int &display)
 
 void VideoWindow::closeEvent (QCloseEvent *event)
 {
-    ((MainWindow * )this->parentWidget())->stop();
     emit(closed());
 }
