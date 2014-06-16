f29fe11301d63b4d52b457a50ae462a8852534ee
DSaunier <saunier.denis.86@gmail.com>
2014-06-16 15:38:46 +0200

    Correct name of installers

--- src/config.h
+++ src/config.h
@@ -36,10 +36,10 @@ namespace config_opp {
 
     #if defined(Q_OS_MAC)
         const QString LINK_UPDATE = "macos/latest/";
-        const QString NAME_INSTALLER = "setup_opp.exe";
+        const QString NAME_INSTALLER = "oppFinal.pkg";
     #elif defined(Q_OS_WIN)
         const QString LINK_UPDATE = "windows/latest/";
-        const QString NAME_INSTALLER = "oppFinal.pkg";
+        const QString NAME_INSTALLER = "setup_opp.exe";
     #elif defined(Q_OS_UNIX)
         const QString LINK_UPDATE = "unix/latest/";
         const QString NAME_INSTALLER = "OPP_1.0-stable_amd64.deb";
