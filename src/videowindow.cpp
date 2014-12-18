cinemaouvert/OPP

commit 6e7b7ec06d43fe678a0e77a8f8531e73a8f65301

Connect playerControlWidget shortcuts to VideoWindow

src/videowindow.cpp
@@ -44,6 +44,7 @@ VideoWindow::VideoWindow(QWidget *parent, const DisplayMode &mode) :
 
     setDisplayMode(mode);
 
+    //TODO Delete !
     _f1_shortcut = new QShortcut(QKeySequence("f1"), this);
     _escape_shortcut = new QShortcut(QKeySequence(tr("escape")), this);
 
@@ -59,11 +60,19 @@ VideoWindow::VideoWindow(QWidget *parent, int width, int height) :
 
     setWindowTitle("Video");
     resize(width, height);
+
+    //TODO : why switchVideoMode and escapeFullScreen are not connected this case?
 }
 
 VideoWindow::~VideoWindow()
 {
     delete _videoWidget;
+
+    delete _playPause_shortcut;
+    delete _previous_shortcut;
+    delete _next_shortcut;
+    delete _rewind_shortcut;
+    delete _forward_shortcut;
 }
 
 void VideoWindow::setDisplayMode(const DisplayMode &mode)
@@ -108,4 +117,49 @@ void VideoWindow::escapeFullscreen()
             ((MainWindow*)parent())->switchVideoMode();
         }
     }
+}
+
+void VideoWindow::initShortcuts()
+{
+    // Possible improvement : create a list of widgets and iterate thrue
+
+    _playPause_shortcut = new QShortcut(
+                ((MainWindow*)parent())->playerControlWidget()->get_playPause_shortcut()->key(),
+                this);
+    connect(_playPause_shortcut,
+            SIGNAL(activated()),
+            ((MainWindow*)parent())->playerControlWidget()->playPauseButton(),
+           SLOT(click()));
+
+    _forward_shortcut = new QShortcut(
+                ((MainWindow*)parent())->playerControlWidget()->get_forward_shortcut()->key(),
+                this);
+    connect(_forward_shortcut,
+            SIGNAL(activated()),
+             ((MainWindow*)parent())->playerControlWidget()->forwardButton(),
+            SLOT(click()));
+
+    _next_shortcut = new QShortcut(
+                ((MainWindow*)parent())->playerControlWidget()->get_next_shortcut()->key(),
+                this);
+    connect(_next_shortcut,
+            SIGNAL(activated()),
+            ((MainWindow*)parent())->playerControlWidget()->nextButton(),
+           SLOT(click()));
+
+    _previous_shortcut = new QShortcut(
+                ((MainWindow*)parent())->playerControlWidget()->get_previous_shortcut()->key(),
+                this);
+    connect(_previous_shortcut,
+            SIGNAL(activated()),
+            ((MainWindow*)parent())->playerControlWidget()->previousButton(),
+           SLOT(click()));
+
+    _rewind_shortcut = new QShortcut(
+                ((MainWindow*)parent())->playerControlWidget()->get_rewind_shortcut()->key(),
+                this);
+    connect(_rewind_shortcut,
+            SIGNAL(activated()),
+            ((MainWindow*)parent())->playerControlWidget()->rewindButton(),
+           SLOT(click()));
 }

src/videowindow.h
@@ -92,6 +92,13 @@ public:
      */
     void closeEvent (QCloseEvent *event);
 
+    /**
+     * @brief Connect the PlayerControlWidget shortcuts to the VideoWindow
+     *
+     * @author Jerome Blanchi <d.j.a.y@free.fr>
+     */
+    void initShortcuts();
+
 private slots:
     void switchVideoMode();
 
@@ -119,6 +126,12 @@ private:
     QShortcut* _f1_shortcut;
 
     QShortcut* _escape_shortcut;
+
+    QShortcut* _playPause_shortcut;
+    QShortcut* _previous_shortcut;
+    QShortcut* _next_shortcut;
+    QShortcut* _rewind_shortcut;
+    QShortcut* _forward_shortcut;
 };
 
 #endif // VIDEOWINDOW_H

src/mainwindow.cpp
@@ -132,6 +132,10 @@ MainWindow::MainWindow(QWidget *parent) :
      */
     _playerControlWidget = new PlayerControlWidget(_playlistPlayer, this);
 
+    // connect playercontrolwidget shortcut to videowindow.
+    // Important : Must be done after _playerControlWidget creation
+    _videoWindow->initShortcuts();
+
     /*********** Media list model and schedule list model *************/
     _mediaListModel = new MediaListModel();
     _scheduleListModel = new ScheduleListModel();

commit b3736cde9aa8906118414ba92d81d7d4142e474f

Fix segfault (introduced by commit 6e7b7ec06d4) when VideoWindow created who occured when created from VideoWindow::VideoWindow(QWidget *parent, int width, int height)

src/videowindow.cpp
@@ -36,10 +36,9 @@
 VideoWindow::VideoWindow(QWidget *parent, const DisplayMode &mode) :
     QMainWindow(parent)
 {
-    _videoWidget = new VideoWidget(this);
-    setCentralWidget(_videoWidget);
+    //Kind of delegate constructor
+    initVideoWindow();
 
-    setWindowTitle("Video");
     resize(640,480);
 
     setDisplayMode(mode);
@@ -55,24 +54,38 @@ VideoWindow::VideoWindow(QWidget *parent, const DisplayMode &mode) :
 VideoWindow::VideoWindow(QWidget *parent, int width, int height) :
     QMainWindow(parent)
 {
+    //Kind of delegate constructor
+    initVideoWindow();
+
+    resize(width, height);
+
+    //switchVideoMode and escapeFullScreen shortcyts are not connected this case
+    //because this constructor is used to generate screenshots
+}
+
+void VideoWindow::initVideoWindow()
+{
+    _playPause_shortcut= NULL;
+    _previous_shortcut= NULL;
+    _next_shortcut= NULL;
+    _rewind_shortcut= NULL;
+    _forward_shortcut= NULL;
+
     _videoWidget = new VideoWidget(this);
     setCentralWidget(_videoWidget);
 
     setWindowTitle("Video");
-    resize(width, height);
-
-    //TODO : why switchVideoMode and escapeFullScreen are not connected this case?
 }
 
 VideoWindow::~VideoWindow()
 {
     delete _videoWidget;
 
-    delete _playPause_shortcut;
-    delete _previous_shortcut;
-    delete _next_shortcut;
-    delete _rewind_shortcut;
-    delete _forward_shortcut;
+    if (_playPause_shortcut) delete _playPause_shortcut;
+    if (_previous_shortcut) delete _previous_shortcut;
+    if (_next_shortcut) delete _next_shortcut;
+    if (_rewind_shortcut) delete _rewind_shortcut;
+    if (_forward_shortcut) delete _forward_shortcut;
 }
 
 void VideoWindow::setDisplayMode(const DisplayMode &mode)

src/videowindow.h
@@ -132,6 +132,11 @@ private:
     QShortcut* _next_shortcut;
     QShortcut* _rewind_shortcut;
     QShortcut* _forward_shortcut;
+
+    /**
+     * @brief initVideoWindow Shared initialization between constructors.
+     */
+    void initVideoWindow();
 };
 
 #endif // VIDEOWINDOW_H